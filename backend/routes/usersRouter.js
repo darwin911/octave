134ee7eaa85a3ead924e710d650b276a9858feaa
Darwin Smith <darwinspsmith@gmail.com>
2019-03-24 03:05:21 -0400

    Adds Post for /users/ to create new user

--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -14,4 +14,14 @@ usersRouter.get('/', async (req, res) => {
   }
 });
 
+usersRouter.post('/', async (req, res) => {
+  try {
+    const user =  await User.create(req.body);
+    res.json({ user });
+  } catch(e) {
+    console.log(e);
+    res.stats(500).send(e.message);
+  }
+});
+
 module.exports = usersRouter
