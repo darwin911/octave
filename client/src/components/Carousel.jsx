bdade138fe103e5c61ef977fd2967e3fdbfbad40
Darwin Smith <darwinspsmith@gmail.com>
2019-03-24 03:04:03 -0400

    Adds Route to render Login at `/`

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import Login from './Login';
 
 const Carousel = () => {
   return (
     <div className="carousel">
+      <Route path="/" render={ () => ( <Login /> ) } />
     </div>
   )
 };
