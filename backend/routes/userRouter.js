11127fd6711a6eb18c7e0f2d7f6062cd622e6907
Eric Sohn <ericsohny@gmail.com>
2019-03-24 21:04:57 -0400

    Add user post and user login functions

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -16,11 +16,64 @@ userRouter.get('/', async (req, res) => {
 
 userRouter.post('/', async (req, res) => {
   try {
-    const user = await User.create(req.body);
-    res.json({ user });
-  } catch (e) {
-    console.log(e);
-    res.stats(500).send(e.message);
+    const { email, password, name } = req.body;
+    const passwordDigest = await hash(password);
+  
+    const user = await User.create({
+      email,
+      password_digest: passwordDigest,
+      name,
+    });
+
+    const userData = {
+      email: user.email,
+      password_digest: user.password_digest,
+      name: user.name,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    const token = encode(userData);
+
+    res.json({
+      token,
+      userData,
+    });
+  } catch(e) {
+    console.error(e);
+  }
+});
+
+userRouter.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (user !== null) {
+      const authenticated = await compare(password, user.password_digest);
+      if (authenticated === true) {
+        const userData = {
+          email: user.email,
+          password_digest: user.password_digest,
+          created_at: user.created_at,
+          updated_at: user.updated_at,
+        };
+
+        const token = encode(userData);
+
+        res.json({
+          token,
+          userData,
+        });
+      }
+      return res.status(401).send('Invalid Credentials');
+    }
+    return res.status(401).send('Invalid Credentials');
+  } catch(e) {
+    console.error(e);
   }
 });
 
