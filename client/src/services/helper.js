0d7e7c770501db52421cb78d7e37ab5d97884daf
MikeyF53 <mikefang530@gmail.com>
2019-03-24 22:55:43 -0400

    added some endpoints

--- client/src/services/helper.js
+++ client/src/services/helper.js
@@ -19,6 +19,38 @@ const getHello = async () => {
   }
 }
 
+// Register
+const createUser = async (data) => {
+  const resp = await api.post(`${BASE_URL}/users`, data);
+  return resp.data;
+};
+// Login
+const loginUser = async (data) => {
+  const resp = await api.post(`${BASE_URL}/users/login`, data);
+  return resp.data;
+}
+// Create review
+const createReview = async (id, data) => {
+  const resp = await api.post(`${BASE_URL}/users/${id}/review`, data);
+  return resp.data;
+};
+// Delete review
+const deleteReview = async (user_id, review_id) => {
+  const resp = await api.delete(`${BASE_URL}/users/${user_id}/review/${review_id}`);
+  return resp.data;
+}
+// Show all events 
+const allEvents = async () => {
+  const resp = await api.get(`${BASE_URL}/events`);
+  return resp.data;
+};
+// Show events on profile page
+const userEvents = async (id) => {
+  const resp = await api.get(`${BASE_URL}/users/${id}/events`);
+  return resp.data;
+};
+
+
 export {
   getHello,
 }
