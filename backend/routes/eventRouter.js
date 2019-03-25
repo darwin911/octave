c26304ac74cfcb8ec5c5a2140671a551ba55addb
Eric Sohn <ericsohny@gmail.com>
2019-03-25 19:37:58 -0400

    Add create and delete methods for artists, events, and venues

--- backend/routes/eventRouter.js
+++ backend/routes/eventRouter.js
@@ -13,4 +13,32 @@ eventRouter.get('/', async (req, res) => {
   }
 });
 
+eventRouter.post('/', async (req, res) => {
+  try {
+    const { title } = req.body;
+    const event = await Event.create({
+      title,
+    });
+    res.json({ event });
+  } catch (e) {
+    console.log(e);
+    res.stats(500).send(e.message);
+  }
+});
+
+eventRouter.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.destroy({
+      where: {
+        id
+      }
+    });
+    res.json({ event });
+  } catch (e) {
+    console.log(e);
+    res.stats(500).send(e.message);
+  }
+});
+
 module.exports = eventRouter;
