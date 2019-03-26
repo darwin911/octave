441d8b7cb5aabb7e82a95778583b5561e0eded21
Darwin Smith <darwinpsmith@gmail.com>
2019-03-26 19:52:53 -0400

    pulls event data  (10) from API and loads into state

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import Reel from './Reel'
+import { loadEvents } from '../services/helper';
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-
+      events: [],
     }
 
   }
 
+  async componentDidMount() {
+    const events = await loadEvents();
+    this.setState({
+      events,
+    })
+  }
+
   render() {
+    const { events } = this.state;
     return (
-      <>
-        <Reel />
-        <Reel />
-      </>
+      <Reel events={events} />
     )
   }
 }
