be90d2e1fee4f363fc8ac44b39fed537c71cc8e6
Akinyemi Ogundipe <ijnuthomefolder@ool-ad02062f.dyn.optonline.net>
2019-03-26 09:08:16 -0400

    review routes

--- backend/routes/artistReviewRouter.js
+++ backend/routes/artistReviewRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { ArtistReview } = require('../models');
-
+const { restrict } = require('../auth');
 const artistReviewRouter = express.Router();
 
 artistReviewRouter.get('/', async (req, res) => {
@@ -12,5 +12,46 @@ artistReviewRouter.get('/', async (req, res) => {
     res.stats(500).send(e.message);
   }
 });
+
+artistReviewRouter.post('/:id/artistreviews', restrict, async (req, res, next) => {
+
+  try {
+    let {content, score} = req.body
+    const userId = req.params.id;
+    const createReview = await ArtistReview.create({
+      content, score, userId
+    })
+    let selectedUser = await User.findOne({
+      where: {
+        id: email
+      }
+    })
+    let resp = await createReview.setUser(selectedUser)
+    res.json(resp)
+  } catch (e) {
+    next(e)
+  }
+})
+
+artistReviewRouter.put(':id/artistreviews/', restrict, async (req, res) => {
+  let {id, content, score} = req.body;
+  try {
+    const review = await ArtistReview.findByPk(id);
+    let updatedReview = await review.update({content, score});
+    res.json(updatedReview);
+  } catch (e) {
+    res.status(500)
+  }
+})
+
+artistReviewRouter.destroy(':id/artistreviews', restrict, async (req, res, next) => {
+    try {
+      const review = await ArtistReview.findByPk(req.params.id);
+      review.destroy();
+      res.status(200).send(`Deleted review with id ${req.params.id}`);
+    } catch (e) {
+      next(e)
+    }
+  })
 
 module.exports = artistReviewRouter;
--- backend/routes/venueReviewRouter.js
+++ backend/routes/venueReviewRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { VenueReview } = require('../models');
-
+const { restrict } = require('../auth');
 const venueReviewRouter = express.Router();
 
 venueReviewRouter.get('/', async (req, res) => {
@@ -12,5 +12,46 @@ venueReviewRouter.get('/', async (req, res) => {
     res.stats(500).send(e.message);
   }
 });
+
+venueReviewRouter.post('/:id/venuereviews', restrict, async (req, res, next) => {
+
+  try {
+    let {content, score} = req.body
+    const userId = req.params.id;
+    const createReview = await VenueReview.create({
+      content, score, userId
+    })
+    let selectedUser = await User.findOne({
+      where: {
+        id: email
+      }
+    })
+    let resp = await createReview.setUser(selectedUser)
+    res.json(resp)
+  } catch (e) {
+    next(e)
+  }
+})
+
+venueReviewRouter.put(':id/venuereviews/', restrict, async (req, res) => {
+  let {id, content, score} = req.body;
+  try {
+    const review = await VenueReview.findByPk(id);
+    let updatedReview = await review.update({content, score});
+    res.json(updatedReview);
+  } catch (e) {
+    res.status(500)
+  }
+})
+
+venueReviewRouter.destroy(':id/venuereviews', restrict, async (req, res, next) => {
+    try {
+      const review = await VenueReview.findByPk(req.params.id);
+      review.destroy();
+      res.status(200).send(`Deleted review with id ${req.params.id}`);
+    } catch (e) {
+      next(e)
+    }
+  })
 
 module.exports = venueReviewRouter;
