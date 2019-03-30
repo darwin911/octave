36ab799a7663cfb8e1a2ab7d69064589d43cbccb
Darwin Smith <darwinpsmith@gmail.com>
2019-03-30 15:50:35 -0400

    refactored components and routes for Auth -> Login/Register

--- client/src/App.js
+++ client/src/App.js
@@ -3,33 +3,82 @@ import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
+import { loginUser } from './services/helper';
+import { withRouter } from 'react-router'
+import decode from 'jwt-decode'
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       user: {
-        name: 'Mike',
+        name: '',
         picture:'',
-        id: 2,
+        id: '',
       },
-      token: ''
+      isLoggedIn: false,
     };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentDidMount() {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      const user = decode(token)
+      this.setState({
+        user,
+        isLoggedIn: true
+       })
+       this.props.history.push('/home')
+    }
+  }
+
+  async handleLogin(userData) {
+    const resp = await loginUser(userData)
+    console.log(resp)
+    localStorage.setItem('token', resp.token)
+    if (resp.token !== null) {
+      this.setState({
+        user: resp.userData,
+        isLoggedIn: true
+       })
+    } else {
+      this.setState({ isLoggedIn: false });
+    }
+    this.props.history.push('/home')
+  }
+
+  handleLogout() {
+    this.setState({
+      user: {
+        name: '',
+        picture:'',
+        id: '',
+      },
+      isLoggedIn: false,
+    })
   }
 
   render() {
+    const { isLoggedIn, token, user } = this.state
     return (
       <div className='App'>
         <Header
+          handleLogout={this.handleLogout}
+          isLoggedIn={isLoggedIn}
           token={this.state.token}
           user={this.state.user} />
         <Main
-          token={this.state.token}
-          user={this.state.user}/>
+          isLoggedIn={isLoggedIn}
+          handleLogin={this.handleLogin}
+          token={token}
+          user={user}/>
         <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,29 +4,25 @@ import { Link } from 'react-router-dom';
 import { dropToken } from '../services/helper';
 
 class Header extends Component{
-  constructor() {
-    super();
-
-  }
 
   goToUserProfile(userId) {
     this.props.history.push(`/user/${userId}`);
   }
 
   render() {
-    const token = localStorage.getItem('authToken')
     return (
      <header>
          <Link to="/home"><h1 className="title">octave.</h1></Link>
        <nav>
        {
-         token
+         this.props.isLoggedIn
          ?
           <>
             <p><Link to="/home">Home</Link></p>
             <p onClick={() => this.goToUserProfile(this.props.user.id)}>{this.props.user.name}</p>
             <p onClick={() => {
               dropToken();
+              this.props.handleLogout();
               this.props.history.push(`/`);
             }}>Sign Out</p>
           </>
