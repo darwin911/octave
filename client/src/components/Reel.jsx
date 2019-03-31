0b0342b5a7fa112044e368b1edc6b17851f9f989
Darwin Smith <darwinpsmith@gmail.com>
2019-03-30 23:05:52 -0400

    updated style and html structure of Reel

--- client/src/components/Reel.jsx
+++ client/src/components/Reel.jsx
@@ -11,30 +11,34 @@ const Reel = props => {
           className='event'
           onClick={e => {
             e.preventDefault();
-            props.handleSetEvent(event);}}>
+            props.handleSetEvent(event);
+          }}>
           {/* Event Image */}
           <a href='../events/:id'>
             <img
+              className="event-img"
               src={event.images.sort((a, b) => b.width - a.width)[4].url}
               alt={event.name}
             />
           </a>
           {/* Event Name */}
-          <p>{event.name}</p>
-          {/* Min/Max Price. If returned from API */}
-          {event.priceRanges && <p>${event.priceRanges[0].min}</p>}
-          {/* Date YYYY/MM/DD */}
-          <p>{moment(event.dates.start.localDate).format('MMM Do, YYYY')}</p>
+          <div>
+            <p>{event.name}</p>
+            {/* Min/Max Price. If returned from API */}
+            {event.priceRanges && <p>${event.priceRanges[0].min}</p>}
+            {/* Date YYYY/MM/DD */}
+            <p>{moment(event.dates.start.localDate).format('MMM Do, YYYY')}</p>
 
-          {event._embedded.venues.map(venue => (
-            <p key={venue.id}>{venue.name}</p>
-          ))}
+            {event._embedded.venues.map(venue => (
+              <p key={venue.id}>{venue.name}</p>
+            ))}
 
-          {event._embedded.venues.map(venue => (
-            <p key={venue.id}>
-              {venue.city.name}, {venue.state.name}
-            </p>
-          ))}
+            {event._embedded.venues.map(venue => (
+              <p key={venue.id}>
+                {venue.city.name}, {venue.state.name}
+              </p>
+            ))}
+          </div>
         </article>
       ))}
     </section>
