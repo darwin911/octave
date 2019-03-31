04b60359aa1258ef6bd840fb5cacbf815c62e8c0
Darwin Smith <darwinpsmith@gmail.com>
2019-03-31 15:54:35 -0400

    updated Reel and HomeNav Style

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,16 @@ import Reel from './Reel';
 const Home = (props) => {
   return (
     <section>
+      <nav className="home-nav">
+        <p>Artists</p>
+        <p>Events</p>
+        <input type="text" name="search" id="search" placeholder="&#128269;" />
+      </nav>
       <h1>Home</h1>
+      <Reel
+        className="reel"
+        events={props.events}
+        handleSetEvent={props.handleSetEvent} />
       <Reel
         className="reel"
         events={props.events}
