71f0f5323a29400cfb22eb0957da0d38a6f6e09a
Darwin Smith <darwinpsmith@gmail.com>
2019-03-31 12:48:05 -0400

    updated styles for Event

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { singleEvent,
+import {
+  singleEvent,
   findEvent,
   findVenue,
   getVenueReviews,
@@ -10,7 +11,8 @@ import { singleEvent,
   addArtist,
   addLike,
   findArtist,
-  getArtistReviews } from '../services/helper';
+  getArtistReviews
+} from '../services/helper';
 import VenueReviewForm from './VenueReviewForm';
 import ArtistReviewForm from './ArtistReviewForm';
 
@@ -45,7 +47,7 @@ class Events extends Component {
     const artist = await findArtist(fetchArtist);
     if (artist.artist) {
       const artistReviews = await getArtistReviews(artist.artist.id);
-      this.setState({ artistReviews})
+      this.setState({ artistReviews })
     }
   }
 
@@ -55,56 +57,71 @@ class Events extends Component {
     return (
       <section>
         <h2>{currentEvent && currentEvent.name}</h2>
-        <article>
+        <article className="selected-event">
           {currentEvent && (
             <>
               <img src={currentEvent.images.sort((a, b) => b.width - a.width)[4].url} alt={currentEvent.name} />
-              <h3>{currentEvent && currentEvent.name}</h3>
-              {currentEvent._embedded.venues.map(venue => (
-                <p key={venue.id}>{venue.name}, {venue.city.name}, {venue.state.name}</p>))}
-              <p>{moment(currentEvent.dates.start.localDate).format('MMM Do, YYYY')}</p>
-              {currentEvent.priceRanges &&
-                <p>Min: ${currentEvent.priceRanges[0].min}</p>}
-              {currentEvent.priceRanges &&
-                <p>Max: ${currentEvent.priceRanges[0].max}</p>}
-              {currentEvent._embedded.attractions.map(artist => (
-                <p key={artist.id}>{artist.name}</p>))}
-              <button onClick={async () => {
-                const fetchArtist = this.state.currentEvent._embedded.attractions[0].name;
-                const artist = await findArtist(fetchArtist);
+              <aside className="event-details">
+                <h3>{currentEvent._embedded.attractions.map(artist => artist.name + ' ')}</h3>
 
-                // will only add an artist to our database if artist does not exist
-                if (artist.artist) {
-                  await addLike(this.props.user.id, artist.artist.id);
-                } else {
-                  const newArtist = await addArtist({name: this.props.currentEvent._embedded.attractions[0].name, picture: this.props.currentEvent._embedded.attractions[0].images[0].url});
-                  await addLike(this.props.user.id, newArtist.artist.id);
-                }
-              }}>Follow</button>
+                <button className="follow-btn" onClick={async () => {
+                  const fetchArtist = this.state.currentEvent._embedded.attractions[0].name;
+                  const artist = await findArtist(fetchArtist);
+                  // will only add an artist to our database if artist does not exist
+                  if (artist.artist) {
+                    await addLike(this.props.user.id, artist.artist.id);
+                  } else {
+                    const newArtist = await addArtist({ name: this.props.currentEvent._embedded.attractions[0].name, picture: this.props.currentEvent._embedded.attractions[0].images[0].url });
+                    await addLike(this.props.user.id, newArtist.artist.id);
+                  }
+                }}>+</button>
+
+                {currentEvent._embedded.venues.map(venue => (
+                  <p key={venue.id}><span className="venue-name">{venue.name}</span>, {venue.city.name}, {venue.state.name}</p>))}
+
+                <p>{moment(currentEvent.dates.start.localDate).format('MMM Do, YYYY')}</p>
+                {currentEvent.priceRanges &&
+                  <p className="buy-tickets">Buy Tickets - ${currentEvent.priceRanges[0].min} - ${currentEvent.priceRanges[0].max}</p>}
+
+                <button
+                  className="attending-btn"
+                  onClick={async () => {
+                    const fetchEvent = this.state.currentEvent.name;
+                    const event = await findEvent(fetchEvent);
+                    // first check to see if event exist in our database
+                    if (event.event) {
+                      await addUserEvent(this.props.user.id, event.event.id);
+                    } else {
+                      const fetchVenue = this.state.currentEvent._embedded.venues[0].name;
+                      const venue = await findVenue(fetchVenue);
+                      // if event does not exist, then checks to see if venue exists in our database
+                      if (venue.venue) {
+                        const newEvent = await addEvent({
+                          title: currentEvent.name,
+                          picture: currentEvent.images[0].url
+                        }, venue.venue.id);
+                        await addUserEvent(
+                          this.props.user.id,
+                          newEvent.event.id);
+                      } else {
+                        const newVenue = await addVenue({
+                          title: currentEvent._embedded.venues[0].name,
+                          picture: currentEvent._embedded.venues[0].images[0].url
+                        });
+                        const newEvent = await addEvent({
+                          title: currentEvent.name,
+                          picture: currentEvent.images[0].url,
+                        }, newVenue.venue.id);
+                        await addUserEvent(
+                          this.props.user.id,
+                          newEvent.event.id,
+                        );
+                      }
+                    }
+                  }}>&#10004;</button><span>Attending</span>
+              </aside>
             </>
           )}
-          <button onClick={async () => {
-            const fetchEvent = this.state.currentEvent.name;
-            const event = await findEvent(fetchEvent);
-
-            // first check to see if event exist in our database
-            if (event.event) {
-              await addUserEvent(this.props.user.id, event.event.id);
-            } else {
-              const fetchVenue = this.state.currentEvent._embedded.venues[0].name;
-              const venue = await findVenue(fetchVenue);
-
-              // if event does not exist, then checks to see if venue exists in our database
-              if (venue.venue) {
-                const newEvent = await addEvent({title: currentEvent.name, picture: currentEvent.images[0].url}, venue.venue.id);
-                await addUserEvent(this.props.user.id, newEvent.event.id);
-              } else {
-                const newVenue = await addVenue({title: currentEvent._embedded.venues[0].name, picture: currentEvent._embedded.venues[0].images[0].url});
-                const newEvent = await addEvent({title: currentEvent.name, picture: currentEvent.images[0].url}, newVenue.venue.id);
-                await addUserEvent(this.props.user.id, newEvent.event.id);
-              }
-            }
-          }}>Attending</button>
         </article>
 
         <h3>Venue Reviews</h3>
--- client/src/components/Reel.jsx
+++ client/src/components/Reel.jsx
@@ -25,10 +25,9 @@ const Reel = props => {
           <div>
             <p>{event.name}</p>
             {/* Min/Max Price. If returned from API */}
-            {event.priceRanges && <p>${event.priceRanges[0].min}</p>}
             {/* Date YYYY/MM/DD */}
             <p>{moment(event.dates.start.localDate).format('MMM Do, YYYY')}</p>
-
+            {event.priceRanges && <p>${event.priceRanges[0].min}</p>}
             {event._embedded.venues.map(venue => (
               <p key={venue.id}>{venue.name}</p>
             ))}
