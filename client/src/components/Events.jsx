b954de631c7d52daace2d268a9619905b48a0eda
Darwin Smith <darwinpsmith@gmail.com>
2019-03-31 20:20:46 -0400

    re-added if statement to events for reviews

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -32,22 +32,28 @@ class Events extends Component {
       currentEvent: event
     })
 
-    const fetchVenue = this.state.currentEvent._embedded.venues[0].name;
-    const venue = await findVenue(fetchVenue);
+    if (this.state.currentEvent) {
 
-    if (venue.venue) {
-      const venueReviews = await getVenueReviews(venue.venue.id);
+      
+      const fetchVenue = this.state.currentEvent._embedded.venues[0].name;
+      const venue = await findVenue(fetchVenue);
 
-      this.setState({
-        venueReviews: venueReviews
-      })
-    }
+      if (venue.venue) {
+        const venueReviews = await getVenueReviews(venue.venue.id);
+        this.setState({
+          venueReviews: venueReviews
+        })
+      }
 
-    const fetchArtist = this.state.currentEvent._embedded.attractions[0].name;
-    const artist = await findArtist(fetchArtist);
-    if (artist.artist) {
-      const artistReviews = await getArtistReviews(artist.artist.id);
-      this.setState({ artistReviews })
+
+      const fetchArtist = this.state.currentEvent._embedded.attractions[0].name;
+      const artist = await findArtist(fetchArtist);
+      if (artist.artist) {
+        const artistReviews = await getArtistReviews(artist.artist.id);
+        this.setState({ artistReviews })
+      }
+
+
     }
   }
 
@@ -125,25 +131,38 @@ class Events extends Component {
           )}
         </article>
 
-        <h3>Venue Reviews</h3>
-        {venueReviews.venueReviews &&
-          <div>{venueReviews.venueReviews.map(venueReview => (
-            <p key={venueReview.id}>{venueReview.userId}, {moment(venueReview.createdAt).format('MMM Do, YYYY')}, {venueReview.content}, {venueReview.score}</p>))}
-          </div>
-        }
-        <VenueReviewForm
-          currentEvent={currentEvent}
-          user={this.props.user} />
+        <section className="reviews">
+          <h4>{currentEvent && currentEvent.name} Reviews</h4>
+          <div className="venue-review">
 
-        <h3>Artist Reviews</h3>
-        {artistReviews &&
-          <div>{artistReviews.map(artistReview => (
-            <p key={artistReview.id}>{artistReview.userId}, {moment(artistReview.createdAt).format('MMM Do, YYYY')}, {artistReview.content}, {artistReview.score}</p>))}
+            {venueReviews.venueReviews && venueReviews.venueReviews.map(review => (
+              <>
+                <p key={review.id}>
+                  <span>UserId: {review.userId},</span> {moment(review.createdAt).format('MMM dddd, YYYY')}
+                </p>
+                <p className="venue-review-content"> "{review.content}" Stars: {review.score}</p>
+              </>
+            ))}
+
+            <VenueReviewForm
+              currentEvent={currentEvent}
+              user={this.props.user} />
           </div>
-        }
-        <ArtistReviewForm
-          currentEvent={currentEvent}
-          user={this.props.user} />
+
+          <div className="artist-review">
+            <h3>Artist Reviews</h3>
+
+            {artistReviews &&
+              <div>{artistReviews.map(artistReview => (
+                <p key={artistReview.id}>{artistReview.userId}, {moment(artistReview.createdAt).format('MMM Do, YYYY')}, {artistReview.content}, {artistReview.score}</p>))}
+              </div>
+            }
+            <ArtistReviewForm
+              currentEvent={currentEvent}
+              user={this.props.user} />
+          </div>
+        </section>
+
       </section>
     );
   };
