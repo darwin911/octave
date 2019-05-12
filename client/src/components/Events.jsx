2e3812eb653c1115645bc94023d03f9a2b8dc71b
Darwin Smith <darwinpsmith@gmail.com>
2019-05-12 09:37:47 -0400

    refactored logic into methods for attendEvent and followArtist in Events

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -27,6 +27,9 @@ class Events extends Component {
       artistReviews: [],
       currentEvent: null
     };
+
+    this.handleAddLike = this.handleAddLike.bind(this);
+    this.handleAttendEvent = this.handleAttendEvent.bind(this);
   }
 
   checkUsernames(userArray, id) {
@@ -83,9 +86,66 @@ class Events extends Component {
     });
   }
 
+  async handleAddLike() {
+      const fetchArtist = this.state.currentEvent._embedded.attractions[0].name;
+      const artist = await findArtist(fetchArtist);
+      // will only add an artist to our database if artist does not exist
+      if (artist.artist) {
+        await addLike(this.props.user.id, artist.artist.id);
+      } else {
+        const newArtist = await addArtist({
+          name: this.props.currentEvent._embedded.attractions[0].name,
+          picture: this.props.currentEvent._embedded.attractions[0].images[0].url
+        });
+        await addLike(this.props.user.id, newArtist.artist.id);
+      }
+  }
+
+  async handleAttendEvent() {
+      const fetchEvent = this.state.currentEvent.name;
+      const event = await findEvent(fetchEvent);
+      // first check to see if event exist in our database
+      if (event.event) {
+        await addUserEvent(this.props.user.id, event.event.id);
+      } else {
+        const fetchVenue = this.state.currentEvent._embedded
+          .venues[0].name;
+        const venue = await findVenue(fetchVenue);
+        // if event does not exist, then checks to see if venue exists in our database
+        if (venue.venue) {
+          const newEvent = await addEvent(
+            {
+              title: this.state.currentEvent.name,
+              picture: this.state.currentEvent.images[0].url
+            },
+            venue.venue.id
+          );
+          await addUserEvent(
+            this.props.user.id,
+            newEvent.event.id
+          );
+        } else {
+          const newVenue = await addVenue({
+            title: this.state.currentEvent._embedded.venues[0].name,
+            picture: this.state.currentEvent._embedded.venues[0].images[0].url
+          });
+          const newEvent = await addEvent(
+            {
+              title: this.state.currentEvent.name,
+              picture: this.state.currentEvent.images[0].url
+            },
+            newVenue.venue.id
+          );
+          await addUserEvent(
+            this.props.user.id,
+            newEvent.event.id
+          );
+        }
+    }
+  }
+
   render() {
     const { currentEvent, venueReviews, artistReviews } = this.state;
-    // const { events, handleSetEvent } = this.props;
 
     return (
       <section className="events">
@@ -104,26 +164,7 @@ class Events extends Component {
 
                 <button
                   className="follow-btn"
-                  onClick={async () => {
-                    const fetchArtist = this.state.currentEvent._embedded
-                      .attractions[0].name;
-                    const artist = await findArtist(fetchArtist);
-                    // will only add an artist to our database if artist does not exist
-                    if (artist.artist) {
-                      await addLike(this.props.user.id, artist.artist.id);
-                    } else {
-                      const newArtist = await addArtist({
-                        name: this.props.currentEvent._embedded.attractions[0]
-                          .name,
-                        picture: this.props.currentEvent._embedded
-                          .attractions[0].images[0].url
-                      });
-                      await addLike(this.props.user.id, newArtist.artist.id);
-                    }
-                  }}
-                >
-                  +
-                </button>
+                  onClick={this.handleAddLike}>+</button>
 
                 {currentEvent._embedded.venues.map(venue => (
                   <p key={venue.id}>
@@ -140,52 +181,7 @@ class Events extends Component {
 
                 <button
                   className="attending-btn"
-                  onClick={async () => {
-                    const fetchEvent = this.state.currentEvent.name;
-                    const event = await findEvent(fetchEvent);
-                    // first check to see if event exist in our database
-                    if (event.event) {
-                      await addUserEvent(this.props.user.id, event.event.id);
-                    } else {
-                      const fetchVenue = this.state.currentEvent._embedded
-                        .venues[0].name;
-                      const venue = await findVenue(fetchVenue);
-                      // if event does not exist, then checks to see if venue exists in our database
-                      if (venue.venue) {
-                        const newEvent = await addEvent(
-                          {
-                            title: currentEvent.name,
-                            picture: currentEvent.images[0].url
-                          },
-                          venue.venue.id
-                        );
-                        await addUserEvent(
-                          this.props.user.id,
-                          newEvent.event.id
-                        );
-                      } else {
-                        const newVenue = await addVenue({
-                          title: currentEvent._embedded.venues[0].name,
-                          picture:
-                            currentEvent._embedded.venues[0].images[0].url
-                        });
-                        const newEvent = await addEvent(
-                          {
-                            title: currentEvent.name,
-                            picture: currentEvent.images[0].url
-                          },
-                          newVenue.venue.id
-                        );
-                        await addUserEvent(
-                          this.props.user.id,
-                          newEvent.event.id
-                        );
-                      }
-                    }
-                  }}
-                >
-                  &#10004;
-                </button>
+                  onClick={this.handleAttendEvent}>&#10004;</button>
                 <span>Attending</span>
 
                 {currentEvent.priceRanges && (
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import Auth from './Auth';
-import { Route } from 'react-router-dom';
-import { withRouter } from 'react-router';
-import HomeDetails from './HomeDetails';
-import Home from './Home';
-import Events from './Events';
-import { allEvents } from '../services/helper';
-import UserProfile from './UserProfile'
+import React, { Component } from "react";
+import Auth from "./Auth";
+import { Route } from "react-router-dom";
+import { withRouter } from "react-router";
+import HomeDetails from "./HomeDetails";
+import Home from "./Home";
+import Events from "./Events";
+import { allEvents } from "../services/helper";
+import UserProfile from "./UserProfile";
 
 class Main extends Component {
   constructor() {
     super();
     this.state = {
       events: [],
-      currentEvent: null,
+      currentEvent: null
     };
     this.handleSetEvent = this.handleSetEvent.bind(this);
   }
 
   async componentDidMount() {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     const events = await allEvents(token);
     this.setState({
       events
@@ -33,38 +33,52 @@ class Main extends Component {
     this.props.history.push(`/events/${ev.id}`);
   }
 
-
   render() {
     return (
       <main>
-        <Route exact path='/' render={() =>
-          <>
-            <Auth
-              handleRegister={this.props.handleRegister}
-              handleLogin={this.props.handleLogin}
-              loginForm={this.props.loginForm} />
-            <HomeDetails />
-          </>} />
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <>
+              <Auth
+                handleRegister={this.props.handleRegister}
+                handleLogin={this.props.handleLogin}
+                loginForm={this.props.loginForm}
+              />
+              <HomeDetails />
+            </>
+          )}
+        />
 
-        <Route path='/home'
-          render={props =>
-            <Home {...props}
+        <Route
+          path="/home"
+          render={props => (
+            <Home
+              {...props}
               events={this.state.events}
-              handleSetEvent={this.handleSetEvent} />} />
+              handleSetEvent={this.handleSetEvent}
+            />
+          )}
+        />
 
-        <Route path='/user/:id'
-          render={() =>
-            <UserProfile
-              user={this.props.user} />} />
+        <Route
+          path="/user/:id"
+          render={() => <UserProfile user={this.props.user} />}
+        />
 
-        <Route path='/events/:id'
-          render={props =>
-            <Events {...props}
+        <Route
+          path="/events/:id"
+          render={props => (
+            <Events
+              {...props}
               user={this.props.user}
               events={this.state.events}
               handleSetEvent={this.handleSetEvent}
-              currentEvent={this.state.currentEvent} />} />
-
+              currentEvent={this.state.currentEvent}
+            />
+          )}
+        />
       </main>
     );
   }
