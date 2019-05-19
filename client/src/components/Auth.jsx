5234ec648969d4ceeba0756446def3b736c62257
Darwin Smith <darwinpsmith@gmail.com>
2019-05-19 15:17:20 -0400

    refactored and update style of Auth

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withRouter } from 'react-router';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router";
 
 class Auth extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      userId: '',
-      name: '',
-      username: '',
-      email: '',
-      password: '',
-      picture: '',
-    }
+      userId: "",
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      picture: ""
+    };
 
     this.handleChange = this.handleChange.bind(this);
   }
@@ -20,13 +20,13 @@ class Auth extends Component {
   handleChange(e) {
     const { name, value } = e.target;
     this.setState({
-      [name]: value,
-    })
+      [name]: value
+    });
   }
 
-  componentClicked = () => console.log('clicked')
+  componentClicked = () => console.log("clicked");
 
-  responseFacebook = (response) => {
+  responseFacebook = response => {
     console.log(response);
     this.setState({
       isLoggedIn: true,
@@ -34,19 +34,18 @@ class Auth extends Component {
       name: response.name,
       email: response.email,
       picture: response.picture.data.url
-    })
-
-  }
+    });
+  };
 
   clearForm() {
     this.setState({
-      userId: '',
-      name: '',
-      username: '',
-      email: '',
-      password: '',
-      picture: '',
-    })
+      userId: "",
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      picture: ""
+    });
   }
 
   render() {
@@ -58,85 +57,90 @@ class Auth extends Component {
         <img src={this.state.picture} alt={this.state.name} />
         <p>Email: {this.state.email}</p>
         <Link to="/home">Continue as {this.state.name}</Link>
-      </div>)
+      </div>
+    );
 
     const userData = {
       email: this.state.email,
       name: this.state.name,
-      password: this.state.password,
-    }
+      password: this.state.password
+    };
 
     return (
       <div className="carousel">
-        <section className="auth">        
-        { this.props.loginForm &&
+        <section className="auth">
+          <h3>Welcome back!</h3>
           <div>
             <form
-              onSubmit={(e) => {
+              onSubmit={e => {
                 e.preventDefault();
-                this.props.handleLogin(userData)
-                this.clearForm()
+                this.props.handleLogin(userData);
+                this.clearForm();
               }}
-              className="login-form">
+              className="login-form"
+            >
+              <label htmlFor="email">Email</label>
               <input
                 className="login-input"
                 type="email"
                 name="email"
-                placeholder="email"
                 onChange={this.handleChange}
                 value={this.state.email}
-                required />
+                required
+              />
+              <label htmlFor="password">Password</label>
               <input
                 className="login-input"
                 type="password"
-                placeholder="Password"
                 name="password"
                 onChange={this.handleChange}
                 value={this.state.password}
-                required />
+                required
+              />
               <button className="sign-in-btn">Sign In</button>
             </form>
-            {fbContent}
           </div>
-        }
-        { !this.props.loginForm &&
+
           <form
-            onSubmit={(e) => {
+            onSubmit={e => {
               e.preventDefault();
-              this.props.handleRegister(userData)
-              this.clearForm()
+              this.props.handleRegister(userData);
+              this.clearForm();
             }}
-            className="register-form">
+            className="register-form"
+          >
+            <label htmlFor="name">Name</label>
             <input
               className="register-input"
               type="text"
               name="name"
-              placeholder="Name"
               onChange={this.handleChange}
               value={this.state.name}
-              required />
+              required
+            />
+            <label htmlFor="email">Email</label>
             <input
               className="register-input"
               type="email"
               name="email"
-              placeholder="email"
               onChange={this.handleChange}
               value={this.state.email}
-              required />
+              required
+            />
+            <label htmlFor="password">Password</label>
             <input
               className="register-input"
               type="password"
-              placeholder="Password"
               name="password"
               onChange={this.handleChange}
               value={this.state.password}
-              required />
-            <button className="sign-up-btn">Sign Up</button>
+              required
+            />
+            <button className="sign-up-btn">Create Account</button>
           </form>
-        }
         </section>
       </div>
-    )
+    );
   }
 }
 
