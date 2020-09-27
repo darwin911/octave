8e5f4267e3d39a61dee9cecee4e2923b2d0c4c8f
Darwin Smith <darwin911@gmail.com>
2020-09-27 16:00:39 -0400

    refactor: spinner accepts size

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export const Spinner = () => <div className='spinner spinner-3' />;
+export const Spinner = ({ size = 100 }) => {
+  return <div className='spinner spinner-3' style={{ width: `${size}px` }} />;
+};
