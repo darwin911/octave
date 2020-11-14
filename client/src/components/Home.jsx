f37bac3eeb55fc7c7267fa82f9463f8d5a7a9b28
Darwin Smith <darwin911@gmail.com>
2020-11-14 15:35:55 -0500

    wip: sorting

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,22 +1,34 @@
+import sortEvents, { sortTypes } from '../util/sortEvents';
+
 import React from 'react';
 import Reel from './Reel';
-import moment from 'moment';
 
-const Home = (props) => {
-  const sortedEvents = [...props.events].sort((ev1, ev2) => {
-    const first = moment(ev1.dates.start.localDate).format('MM DD YYYY');
-    const second = moment(ev2.dates.start.localDate).format('MM DD YYYY');
-    if (first < second) {
-      return -1;
-    } else {
-      return 1;
-    }
-  });
+// import moment from 'moment';
+
+const Home = ({ events }) => {
+  // const sortedEvents = [...events].sort((ev1, ev2) => {
+  //   const first = moment(ev1.dates.start.localDate).format('MM DD YYYY');
+  //   const second = moment(ev2.dates.start.localDate).format('MM DD YYYY');
+  //   if (first < second) {
+  //     return -1;
+  //   } else {
+  //     return 1;
+  //   }
+  // });
 
   return (
     <section className='home'>
-      <Reel heading='UPCOMING EVENTS' className='reel' events={sortedEvents.slice(0, 10)} />
-      <Reel heading='EVENTS IN NYC' className='reel' events={props.events} />
+      {events && events.length > 0 && (
+        <>
+          <Reel
+            heading='UPCOMING EVENTS'
+            className='reel'
+            events={sortEvents(events, sortTypes.MOST_RECENT).slice(0, 10)}
+          />
+
+          <Reel heading='EVENTS IN NYC' className='reel' events={events} />
+        </>
+      )}
     </section>
   );
 };
--- client/src/components/Reel.jsx
+++ client/src/components/Reel.jsx
@@ -1,48 +1,79 @@
+import sortEvents, { sortTypes } from '../util/sortEvents';
+
 import { Link } from 'react-router-dom';
 import React from 'react';
 import moment from 'moment';
 
-const Reel = (props) => {
-  const { className, heading, events } = props;
+const Reel = ({ className, heading, events }) => {
+  const [sortBy, setSortBy] = React.useState(sortTypes.MOST_RECENT);
+  const [sortedEvents, setSortedEvents] = React.useState(events);
+  const handleChange = (ev) => {
+    const {
+      target: { value },
+    } = ev;
+    setSortBy(value);
+  };
+
+  React.useEffect(() => {
+    console.log('USE EFFECT - EVENTS');
+    if (sortedEvents && sortedEvents.length && events && events.length) {
+      // setSortedEvents(sortEvents(events, sortBy));
+    }
+  }, [sortBy]);
+
+  if (!sortedEvents) {
+    return null;
+  }
+
   return (
     <section className={className}>
       <header>
         <h3>{heading}</h3>
-        <select className='filter' id='event-filter-select'>
-          <option value='most-recent'>Sort By</option>
-          <option value='lowest-price'>Lowest Price</option>
-          <option value='highest-price'>Highest Price</option>
-        </select>
-      </header>
-      {events.map((event) => (
-        <div key={event.id} className='event'>
-          <Link to={`/events/${event.id}`} className='event-link-wrapper'>
-            {/* Event Image */}
-            <img
-              className='event-img'
-              src={event.images.sort((a, b) => b.width - a.width)[5].url}
-              alt={event.name}
-            />
-            {/* Event Name */}
-            <div>
-              <p>{event.name}</p>
-              {/* Min/Max Price. If returned from API */}
-              {/* Date YYYY/MM/DD */}
-              <p>{moment(event.dates.start.localDate).format('MMM Do, YYYY')}</p>
-              <p>{event.priceRanges ? '$' + event.priceRanges[0].min : 'SOLD OUT'}</p>
-              {event._embedded.venues.map((venue) => (
-                <p key={venue.id}>{venue.name}</p>
-              ))}
-
-              {event._embedded.venues.map((venue) => (
-                <p key={venue.id}>
-                  {venue.city.name}, {venue.state.stateCode}
-                </p>
-              ))}
-            </div>
-          </Link>
+        <div>
+          <label htmlFor='event-filter-select'>
+            <sub>Sort By</sub>
+          </label>
+          <select
+            className='filter'
+            id='event-filter-select'
+            value={sortBy}
+            onChange={handleChange}>
+            <option value={sortTypes.MOST_RECENT}>Most Recent</option>
+            <option value={sortTypes.LOWEST_PRICE}>Lowest Price</option>
+            <option value={sortTypes.HIGHEST_PRICE}>Highest Price</option>
+          </select>
         </div>
-      ))}
+      </header>
+      {sortedEvents.length > 0 &&
+        sortedEvents.map((event) => (
+          <div key={event.id} className='event'>
+            <Link to={`/events/${event.id}`} className='event-link-wrapper'>
+              {/* Event Image */}
+              <img
+                className='event-img'
+                src={event.images.sort((a, b) => b.width - a.width)[5].url}
+                alt={event.name}
+              />
+              {/* Event Name */}
+              <div>
+                <p>{event.name}</p>
+                {/* Min/Max Price. If returned from API */}
+                {/* Date YYYY/MM/DD */}
+                <p>{moment(event.dates.start.localDate).format('MMM Do, YYYY')}</p>
+                <p>{event.priceRanges ? '$' + event.priceRanges[0].min : 'SOLD OUT'}</p>
+                {event._embedded.venues.map((venue) => (
+                  <p key={venue.id}>{venue.name}</p>
+                ))}
+
+                {event._embedded.venues.map((venue) => (
+                  <p key={venue.id}>
+                    {venue.city.name}, {venue.state.stateCode}
+                  </p>
+                ))}
+              </div>
+            </Link>
+          </div>
+        ))}
     </section>
   );
 };
